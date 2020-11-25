f50db03f83d46c640c700076dd13d9e9ba28ed4b
charlesZ-byte <zsmlvx@gmail.com>
2020-11-24 20:53:32 -0800

    2020/11/24-20:53

--- src/Row.js
+++ src/Row.js
@@ -1,25 +1,36 @@
-import React ,{useState, useEffect}from 'react'
-import axios from './axios'
-function Row({title,fetchUrl}) {
-    const [movies, setMovies] = useState([])
+import React, { useState, useEffect } from "react";
+import axios from "./axios";
 
-    useEffect(() => {
-        async function fetchData(){
-            const request = await axios.get(fetchUrl);
-            console.log(request);
-            return request;
-        }
-        fetchData();
-    }, []);
+const base_url = "https://image.tmdb.org/t/p/original/";
 
+function Row({ title, fetchUrl }) {
+  const [movies, setMovies] = useState([]);
 
-    return (
-        <div>
-            <h2>{title}</h2>
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(fetchUrl);
+      setMovies(request.data.results);
+      return request;
+    }
+    fetchData();
+  }, [fetchUrl]);
 
-            {/*container*/}
-        </div>
-    )
+  console.log(movies);
+  return (
+    <div className="row">
+      <h2>{title}</h2>
+      <div className="row_posters">
+        {movies.map((movie) => (
+          <img
+            className="row_poster"
+            src={`${base_url}${movie.poster_path}`}
+            alt={movie.name}
+          />
+        ))}
+      </div>
+      {/*container*/}
+    </div>
+  );
 }
 
 export default Row;
